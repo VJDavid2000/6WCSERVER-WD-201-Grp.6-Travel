54a6e1d9d20be5fd18c4746572e75b4812425396
VJDavid2000 <98504050+VJDavid2000@users.noreply.github.com>
2022-05-01 21:32:25 +0800

    Add files via upload

---
+++ finalsprj-mevn/src/components/ListComponent.vue
@@ -0,0 +1,73 @@
+<!--
+Name: David, Vincent Jericho C.
+WD-201 (GROUP 6)
+-->
+
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Client Name</th>
+                        <th>Travel Location</th>
+                        <th>Travel Tag No.</th>
+                        <th>Phone Number</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="student in Students" :key="student._id">
+                        <td>{{ travel.name }}</td>
+                        <td>{{ travel.location }}</td>
+                        <td>{{ travel.num }}</td>
+                        <td>{{ travel.phone }}</td>
+                        <td>
+                            <router-link :to="{name: 'edit', params: { id: travel._id }}" class="btn btn-success">Change Details
+                            </router-link>
+
+                            <button @click.prevent="deleteTravel(travel._id)" class="btn btn-danger">Delete Form</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        data() {
+            return {
+                Travels: []
+            }
+        },
+        created() {
+            let apiURL = 'http://localhost:4000/api';
+            axios.get(apiURL).then(res => {
+                this.Travels = res.data;
+            }).catch(error => {
+                console.log(error)
+            });
+        },
+        methods: {
+            deleteTravel(id){
+                let apiURL = `http://localhost:4000/api/delete-travel/${id}`;
+                let indexOfArrayItem = this.Travels.findIndex(i => i._id === id);
+                if (window.confirm("Do You Wish To Clear Out This Form?")) {
+                    axios.delete(apiURL).then(() => {
+                        this.Travels.splice(indexOfArrayItem, 1);
+                    }).catch(error => {
+                        console.log(error)
+                    });
+                }
+            }
+        }
+    }
+</script>
+<style>
+    .btn-success {
+        margin-right: 10px;
+    }
+</style>
